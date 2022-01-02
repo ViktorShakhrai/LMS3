a988d59f598a42954eb34cb25c063413763e1241
Viktor <vikto1759@gmail.com>
2022-01-02 13:47:56 +0200

    comments

--- task2.py
+++ task2.py
@@ -1,3 +1,6 @@
+# Manipulate strings.
+# Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
+
 f_name = "Viktor"
 l_name = "Shakhrai"
 print(f_name+" "+l_name+' ''Привіт,як пройшов твій день?')
